eaadcca430692bcc5508e38bc97c20435b0c6cc4
zaidsheriff786 <mszaid.007@gmail.com>
2023-05-18 10:19:08 +0530

    updating cors url

--- src/constants.js
+++ src/constants.js
@@ -5,13 +5,13 @@ export const MENU_IMG_CDN_URL =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/';
 
 export const RESTAURANTS_URL =
-  'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING';
+  'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING';
 
 export const FETCH_MENU_URL =
-  'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
+  'https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
 
 export const OFFERS_URL =
-  'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/offers/payment?lat=12.913397&lng=77.559001&offset=0';
+  'https://corsproxy.io/?https://www.swiggy.com/dapi/offers/payment?lat=12.913397&lng=77.559001&offset=0';
 
 export const OFFER_IMG_URL =
   'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_40,h_40/';
